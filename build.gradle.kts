4ed57c195461838bbac96503abcdc27ea3353469
Boot750 <beat.baechi@outlook.com>
2025-12-16 18:44:21 +0800

    Added plugin icond and fixed startup issue.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "org.nanoya"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/org/nanoya/terminalmanager/TerminalGearActionInstaller.kt
+++ src/main/kotlin/org/nanoya/terminalmanager/TerminalGearActionInstaller.kt
@@ -1,8 +1,11 @@
 package org.nanoya.terminalmanager
 
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.wm.ToolWindowManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.plugins.terminal.TerminalToolWindowFactory
 import org.nanoya.terminalmanager.actions.OpenSettingsAction
 import org.nanoya.terminalmanager.actions.ResetTerminalsAction
@@ -14,7 +17,9 @@ class TerminalGearActionInstaller : ProjectActivity {
         val terminalToolWindow = toolWindowManager.getToolWindow(TerminalToolWindowFactory.TOOL_WINDOW_ID)
 
         terminalToolWindow?.let { toolWindow ->
-            toolWindow.setTitleActions(listOf(ResetTerminalsAction(), OpenSettingsAction()))
+            withContext(Dispatchers.EDT) {
+                toolWindow.setTitleActions(listOf(ResetTerminalsAction(), OpenSettingsAction()))
+            }
         }
     }
 }
