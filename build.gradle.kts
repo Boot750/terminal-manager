6360e775712a7a7a920e9705a51ca2f7b039fa68
Boot750 <beat.baechi@outlook.com>
2025-12-30 11:16:11 +0800

    Added verified and automatic upload to jetbrains.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.1.10"
@@ -25,10 +27,52 @@ dependencies {
     intellijPlatform {
         intellijIdeaCommunity("2024.3.2")
         bundledPlugin("org.jetbrains.plugins.terminal")
+
+        // Plugin Verifier - test against multiple IDE versions
+        pluginVerifier()
     }
     compileOnly("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("com.google.code.gson:gson:2.11.0")
     testImplementation(kotlin("test"))
+}
+
+intellijPlatform {
+    pluginVerification {
+        // IDE versions to verify against (matching sinceBuild 243 to untilBuild 253.*)
+        ides {
+            // IntelliJ IDEA
+            ide(IntelliJPlatformType.IntellijIdeaCommunity, "2024.3.2")
+            ide(IntelliJPlatformType.IntellijIdeaUltimate, "2024.3.2")
+
+            // WebStorm
+            ide(IntelliJPlatformType.WebStorm, "2024.3.2")
+
+            // PyCharm
+            ide(IntelliJPlatformType.PyCharmCommunity, "2024.3.2")
+            ide(IntelliJPlatformType.PyCharmProfessional, "2024.3.2")
+
+            // PhpStorm
+            ide(IntelliJPlatformType.PhpStorm, "2024.3.2")
+
+            // Rider
+            ide(IntelliJPlatformType.Rider, "2024.3.2")
+
+            // GoLand
+            ide(IntelliJPlatformType.GoLand, "2024.3.2")
+
+            // RubyMine
+            ide(IntelliJPlatformType.RubyMine, "2024.3.2")
+
+            // CLion
+            ide(IntelliJPlatformType.CLion, "2024.3.2")
+
+            // DataGrip
+            ide(IntelliJPlatformType.DataGrip, "2024.3.2")
+
+            // RustRover
+            ide(IntelliJPlatformType.RustRover, "2024.3.2")
+        }
+    }
 }
 
 tasks {
@@ -46,6 +90,6 @@ tasks {
         password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
     }
     publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+        token.set(System.getenv("JETBRAINS_TOKEN"))
     }
 }
